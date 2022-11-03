135b36bb5484756daeaa626b28bbed519af7cec7
AlexandreGDL <guindeuilalexandre@gmail.com>
2022-11-03 17:24:54 +0100

    ✨Test : Add testing component for workflow

---
+++ src/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components/native';
+
+const StyledView = styled.View`
+  background-color: papayawhip;
+`;
+
+const StyledText = styled.Text`
+  color: palevioletred;
+`;
+
+export default () => {
+  return (
+    <StyledView>
+      <StyledText>Hello World!</StyledText>
+    </StyledView>
+  );
+};
